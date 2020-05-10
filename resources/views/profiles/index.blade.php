5f54de0e474e7b63670d5c41f19a71b8d332290e
Jan Koupil <jan.koupil@gmail.com>
2020-05-10 23:06:00 +0200

    Show posts from database (2:19)

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -8,10 +8,10 @@
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline"><h1>{{ $user->username }}</h1>
-                <a href="#">Add new post</a>
+                <a href="/p/create">Add new post</a>
             </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>{{$user->posts->count()}}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
@@ -22,15 +22,13 @@
     </div>
 
     <div class="row pt-5">
-        <div class="col-4">
-            <img src="https://scontent-otp1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c2.0.745.745a/s640x640/95224410_237873460790480_2374979867183673557_n.jpg?_nc_ht=scontent-otp1-1.cdninstagram.com&_nc_cat=108&_nc_ohc=FfvHtrIvI7MAX_NQVpF&oh=a6a655bd4121e8e8f61885102e3ce26a&oe=5ED39EA8" class="w-100" alt="fcc">
-        </div>
-        <div class="col-4">
-            <img src="https://scontent-otp1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/94892216_228412868421097_4113969397730546378_n.jpg?_nc_ht=scontent-otp1-1.cdninstagram.com&_nc_cat=101&_nc_ohc=NaBk0OlvCYQAX-sByl8&oh=0ad811fed010b0e99346a9827ec2fe0e&oe=5ED2D04E" class="w-100" alt="fcc">
-        </div>
-        <div class="col-4">
-            <img src="https://scontent-otp1-1.cdninstagram.com/v/t51.2885-15/e35/c94.0.561.561a/94219713_705997580137437_6728704239228889450_n.jpg?_nc_ht=scontent-otp1-1.cdninstagram.com&_nc_cat=109&_nc_ohc=6jd-Tst18KIAX-0pq12&oh=acf74b7b29637e41aff868b1ed5e42be&oe=5ED2C48F" class="w-100" alt="fcc">
-        </div>
+
+        @foreach( $user->posts as $post )
+            <div class="col-4 pb-4">
+                <img src="/storage/{{$post->image}}" class="w-100" alt="fcc">
+            </div>
+     @endforeach
+
     </div>
 </div>
 @endsection
