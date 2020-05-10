804e53a5d608573c4e767fa5f802fac79437db23
Jan Koupil <jan.koupil@gmail.com>
2020-05-10 21:16:42 +0200

    create Posts table and model

--- app/User.php
+++ app/User.php
@@ -37,6 +37,17 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function posts()
+    {
+        return $this->hasMany((Post::class));
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
     public function profile(){
         return $this->hasOne(Profile::class);
     }
