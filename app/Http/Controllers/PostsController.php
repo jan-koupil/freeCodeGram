a3f44182b18c067df3a939780286a568030f4281
Jan Koupil <jan.koupil@gmail.com>
2020-05-10 22:54:58 +0200

    Add logic to store post and its image

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create()
     {
         return view('posts.create');
@@ -13,6 +19,18 @@ class PostsController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        $data = request()->validate([
+                'caption' =>'required',
+                'image' => ['required','image'],
+        ]);
+
+        $imagePath = \request('image')->store('uploads', 'public');
+
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath
+        ]);
+
+        return redirect('/profile/' . auth()->user()->id);
     }
 }
