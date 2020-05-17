b99be2ec5f032bee4081012583a61c9fed5ab963
jan-koupil <jan.koupil@gmail.com>
2020-05-17 23:06:47 +0200

    Add default profile image (3:16)

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -5,9 +5,9 @@
         <div class="row">
             <div class="col-3 p-5">
                 <img
-                    src="/storage/{{ $user->profile->image }}"
-                    .
-                    alt="FCC Logo" class="rounded-circle w-100">
+                    src="{{ $user->profile->profileImage() }}"
+                    alt="FCC Logo" class="rounded-circle w-100"
+                >
             </div>
             <div class="col-9 pt-5">
                 <div class="d-flex justify-content-between align-items-baseline"><h1>{{ $user->username }}</h1>
