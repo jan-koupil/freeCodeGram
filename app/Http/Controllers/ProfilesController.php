c03d268f1f4a46c9c06be90a7b4d4d8296c24178
jan-koupil <jan.koupil@gmail.com>
2020-05-18 11:19:56 +0200

    Update profile no longer overwrites image if no new submitted

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -51,11 +51,13 @@ class ProfilesController extends Controller
 
             $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000,1000);
             $image->save();
+
+            $imageArray = ['image' => $imagePath];
         }
 
         auth()->user()->profile->update(array_merge(
             $data,
-            ['image' => $imagePath ?? $user->profile->image]
+            $imageArray ?? []
         ));
 
         return redirect("/profile/{$user->id}");
