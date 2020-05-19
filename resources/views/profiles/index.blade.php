c42c81fc6ab1261405a5882c9670cd7e8bd2992f
Jan Koupil <jan.koupil@gmail.com>
2020-05-19 17:44:48 +0200

    update dependencies

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -10,7 +10,12 @@
                 >
             </div>
             <div class="col-9 pt-5">
-                <div class="d-flex justify-content-between align-items-baseline"><h1>{{ $user->username }}</h1>
+                <div class="d-flex justify-content-between align-items-baseline">
+                    <div class="d-flex align-items-center pb-3">
+                        <div class="h4">{{ $user->username }}</div>
+                        <follow-button></follow-button>
+                    </div>
+
                     @can('update', $user->profile)
                         <a href="/p/create">Add new post</a>
                     @endcan
